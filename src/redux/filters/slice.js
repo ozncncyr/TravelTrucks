ae09e1537fb3745755ad6b55730e908569d3fe9c
Ozan Can CUYAR <ozncncyr@outlook.com>
2026-02-25 15:57:10 +0300

    feat: add filters slice and selector for managing filter state

--- src/redux/filters/slice.js
+++ src/redux/filters/slice.js
@@ -1,1 +1,28 @@
-// filters slice
+import { createSlice } from "@reduxjs/toolkit";
+
+export const initialState = {
+  filters: {
+    AC: false,
+    Automatic: false,
+    Kitchen: false,
+    TV: false,
+    Bathroom: false,
+    Van: false,
+    FullyIntegrated: false,
+    Alcove: false,
+    location: null,
+  },
+};
+
+const filtersSlicer = createSlice({
+  name: "filters",
+  initialState,
+  reducers: {
+    setFilter: (state, action) => {
+      state.filters = action.payload;
+    },
+  },
+});
+
+export const { setFilter } = filtersSlicer.actions;
+export default filtersSlicer.reducer;
