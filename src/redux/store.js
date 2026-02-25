2cae123c14c8e98927850bbde65edbdcd375ddbc
Ozan Can CUYAR <ozncncyr@outlook.com>
2026-02-25 15:59:05 +0300

    feat: wrap App component with Provider and BrowserRouter in main.jsx

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,13 +1,14 @@
-// import { configureStore, combineReducers } from "@reduxjs/toolkit";
-// import { persistStore, persistReducer } from "redux-persist";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
-// // import storage from "redux-persist/lib/storage";
-// // import trucksSlicer from "./trucks/slicer.js";
-// // import filtersSlicer from "./filters/slicer.js";
-// // import isFavoriteSlicer from "./isFavorite/slicer.js";
+import trucksSlicer from "./trucks/slicer.js";
+import filtersSlicer from "./filters/slicer.js";
+import isFavoriteSlicer from "./isFavorite/slicer.js";
 
-// const rootReducer = combineReducers({
-//   trucks: trucksSlicer,
-//   filters: filtersSlicer,
-//   isFavorite: isFavoriteSlicer,
-// });
+const rootReducer = combineReducers({
+  trucks: trucksSlicer,
+  filters: filtersSlicer,
+  // reviews: reviewsSlicer,
+  isFavorite: isFavoriteSlicer,
+});
