302d5cab327b09d6060d4f039c5c144cd9627d33
Ozan Can CUYAR <ozncncyr@outlook.com>
2026-02-27 17:28:35 +0300

    feat: add Loader component with styling and integrate react-loader-spinner

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,7 +1,18 @@
-import React from "react";
+import { RevolvingDot } from "react-loader-spinner";
+import css from "./Loader.module.css";
 
-const Loader = () => {
-  return <div></div>;
-};
-
-export default Loader;
+export default function Loader() {
+  return (
+    <div className={css.container}>
+      <RevolvingDot
+        visible={true}
+        height="80"
+        width="80"
+        color="#E44848"
+        ariaLabel="revolving-dot-loading"
+        wrapperStyle={{}}
+        wrapperClass=""
+      />
+    </div>
+  );
+}
