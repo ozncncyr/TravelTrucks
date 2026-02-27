8f9e8ed4a2e4453929eb5c6ec2ceb28e8a9570b6
Ozan Can CUYAR <ozncncyr@outlook.com>
2026-02-27 17:48:07 +0300

    feat: implement VehicleCard component with styling and functionality

---
+++ src/components/VehicleCard/VehicleCard.jsx
@@ -0,0 +1,82 @@
+import css from "./VehicleCard.module.css";
+import "../../index.css";
+import { Link } from "react-router-dom";
+import sprite from "../../images/icons.svg";
+import MenuTruckRateLoc from "../MenuTruckRateLoc/MenuTruckRateLoc.jsx";
+import { setIsFavorite } from "../../redux/isFavorite/slicer.js";
+import { useDispatch, useSelector } from "react-redux";
+import { selectorIsFavorite } from "../../redux/isFavorite/selectors.js";
+
+export default function VehicleCard({ truck }) {
+  const dispatch = useDispatch();
+  const favorites = useSelector(selectorIsFavorite);
+
+  if (!truck) return null;
+  const booleanFavorite = favorites.includes(Number(truck.id));
+
+  const objectIcons = {
+    AC: "AC",
+    TV: "TV",
+    bathroom: "Bathroom",
+    kitchen: "Kitchen",
+  };
+
+  const handleFavoriteClick = () => {
+    dispatch(setIsFavorite(truck.id));
+  };
+
+  return (
+    <div className={css.vehicleCard}>
+      <h2 className="visually-hidden">Trucks List</h2>
+      <div className={css.truckImage}>
+        {truck.gallery && truck.gallery.length > 0 && (
+          <img src={truck.gallery[0].thumb} alt={`${truck.name} image`} />
+        )}
+      </div>
+
+      <div className={css.truckHeader}>
+        <h3 className={css.truckName}>{truck.name}</h3>
+        <div className={css.truckPriceContainer}>
+          <p className={css.truckPrice}>€{truck.price}</p>
+          <svg
+            className={`${css.svgBorder} ${
+              booleanFavorite ? css.svgBorderActive : ""
+            }`}
+            onClick={handleFavoriteClick}
+            width="25"
+            height="24"
+          >
+            <use
+              xlinkHref={`${sprite}#${
+                booleanFavorite ? "Heart-active" : "Heart"
+              }`}
+            />
+          </svg>
+        </div>
+      </div>
+      <MenuTruckRateLoc truck={truck} to={`/catalog/${truck.id}`} />
+
+      <p className={css.truckDescription}>{truck.description}</p>
+
+      <ul className={css.truckFeatures}>
+        {Object.entries(objectIcons).map(
+          ([key, name]) =>
+            truck[key] && (
+              <li className={css.featureItem} key={key}>
+                <svg className={css.featureIcon}>
+                  <use xlinkHref={`${sprite}#${name}`} />
+                </svg>
+                <p className={css.featureItemName}>{name}</p>
+              </li>
+            ),
+        )}
+      </ul>
+
+      <div className={css.showMoreButtonContainer}>
+        <Link className={css.showMoreButton} to={`/catalog/${truck.id}`}>
+          Show more
+        </Link>
+      </div>
+    </div>
+  );
+}
