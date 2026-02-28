2ac650121a49c63b836f0bce7616eafd9748a117
Ozan Can CUYAR <ozncncyr@outlook.com>
2026-02-28 23:11:38 +0300

    feat: refactor CamperReviews component to display reviews with improved layout

--- src/components/CamperReviews/CamperReviews.jsx
+++ src/components/CamperReviews/CamperReviews.jsx
@@ -1,12 +1,27 @@
-import React from "react";
+import "../../index.css";
 import css from "./CamperReviews.module.css";
+import { useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import { selectTrucks } from "../../redux/trucks/selectors.js";
 
-export default function CamperReviews({ reviews }) {
+export default function CamperReviews() {
+  const { camperId } = useParams();
+  const { reviews } = useSelector(selectTrucks)[camperId - 1];
+
   return (
-    <div className={css.container}>
-      <h2>Camper Reviews</h2>
-      {/* Yorumlar burada listelenecek */}
-      {reviews && <pre>{JSON.stringify(reviews, null, 2)}</pre>}
-    </div>
+    <li className={css.reviewSection}>
+      <h2 className="visually-hidden">Reviews</h2>
+      {reviews.map((review) => (
+        <div key={review.reviewer_name} className={css.reviewContainer}>
+          <div className={css.avatar}>{review.reviewer_name.charAt(0)}</div>
+          <div className={css.reviewContent}>
+            <h3 className={css.reviewName}>
+              {review.reviewer_name}, {review.reviewer_rating} stars
+            </h3>
+            <p className={css.reviewComment}>{review.comment}</p>
+          </div>
+        </div>
+      ))}
+    </li>
   );
 }
