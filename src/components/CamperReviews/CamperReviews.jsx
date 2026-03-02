a1d907b3d1509d61e0606ea6db563a24aeb5cc4d
Ozan Can CUYAR <ozncncyr@outlook.com>
2026-03-03 00:11:16 +0300

    feat: add star rating display in CamperReviews and update styles for consistency

--- src/components/CamperReviews/CamperReviews.jsx
+++ src/components/CamperReviews/CamperReviews.jsx
@@ -3,6 +3,7 @@ import css from "./CamperReviews.module.css";
 import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { selectTrucks } from "../../redux/trucks/selectors.js";
+import sprite from "../../assets/icons.svg";
 
 export default function CamperReviews() {
   const { camperId } = useParams();
@@ -16,7 +17,23 @@ export default function CamperReviews() {
           <div className={css.avatar}>{review.reviewer_name.charAt(0)}</div>
           <div className={css.reviewContent}>
             <h3 className={css.reviewName}>
-              {review.reviewer_name}, {review.reviewer_rating} stars
+              {review.reviewer_name}{" "}
+              {Array.from({ length: review.reviewer_rating }).map((_, i) => (
+                <svg
+                  key={i}
+                  className={css.starIcon}
+                  width="16"
+                  height="16"
+                  style={{
+                    marginLeft: 2,
+                    marginRight: 2,
+                    marginBottom: 2,
+                    verticalAlign: "middle",
+                  }}
+                >
+                  <use xlinkHref={`${sprite}#icon-rating`} />
+                </svg>
+              ))}
             </h3>
             <p className={css.reviewComment}>{review.comment}</p>
           </div>
